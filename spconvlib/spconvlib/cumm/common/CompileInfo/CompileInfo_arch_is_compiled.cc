21876e61933f8f590c03da2c62896e1c2473a7d0
Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
2025-01-06 19:30:50 +0900

    fix: fixed error in which only 8.6 gpus could be used. added 8.9 to the list
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>

--- spconvlib/spconvlib/cumm/common/CompileInfo/CompileInfo_arch_is_compiled.cc
+++ spconvlib/spconvlib/cumm/common/CompileInfo/CompileInfo_arch_is_compiled.cc
@@ -3,11 +3,25 @@ namespace spconvlib {
 namespace cumm {
 namespace common {
 bool CompileInfo::arch_is_compiled(std::tuple<int, int> arch)   {
-  
-  
+
+#ifdef CUDA_ARCH_75
+  if (arch == std::make_tuple(7, 5)){
+      return true;
+  }
+#endif
+
+#ifdef CUDA_ARCH_86
   if (arch == std::make_tuple(8, 6)){
       return true;
   }
+#endif
+
+#ifdef CUDA_ARCH_89
+  if (arch == std::make_tuple(8, 9)){
+      return true;
+  }
+#endif
+
   return false;
 }
 } // namespace common
--- spconvlib/spconvlib/cumm/common/CompileInfo/CompileInfo_get_compiled_cuda_arch.cc
+++ spconvlib/spconvlib/cumm/common/CompileInfo/CompileInfo_get_compiled_cuda_arch.cc
@@ -5,7 +5,19 @@ namespace common {
 std::vector<std::tuple<int, int>> CompileInfo::get_compiled_cuda_arch()   {
   
   std::vector<std::tuple<int, int>> res;
+
+#ifdef CUDA_ARCH_75
+  res.push_back(std::make_tuple(7, 5));
+#endif
+
+#ifdef CUDA_ARCH_86
   res.push_back(std::make_tuple(8, 6));
+#endif
+
+#ifdef CUDA_ARCH_89
+  res.push_back(std::make_tuple(8, 9));
+#endif
+
   return res;
 }
 } // namespace common
